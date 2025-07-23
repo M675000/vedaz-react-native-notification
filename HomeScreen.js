e98f6e207df07e6ca5a210745412a66bd89ac8a6
Mohammad Saleemunnisa <salmamd622@gmail.com>
2025-07-23 19:44:29 +0530

    Update HomeScreen.js

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,1 +1,20 @@
-// Home screen component
+import React from 'react';
+import { View, Text, Button, Alert } from 'react-native';
+
+export default function HomeScreen({ navigation }) {
+  const simulatePush = () => {
+    Alert.alert("Incoming Call", "Simulated push notification", [
+      {
+        text: "Answer",
+        onPress: () => navigation.navigate('Notification')
+      }
+    ]);
+  };
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button title="Simulate Call Notification" onPress={simulatePush} />
+    </View>
+  );
+}
