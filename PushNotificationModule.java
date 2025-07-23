f21a8a8c6dccf18d28c2d6f4c4a444857707219e
Mohammad Saleemunnisa <salmamd622@gmail.com>
2025-07-23 19:45:27 +0530

    Update PushNotificationModule.java

--- PushNotificationModule.java
+++ PushNotificationModule.java
@@ -1,1 +1,22 @@
-// Java native module
+package com.yourapp;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import android.util.Log;
+
+public class PushNotificationModule extends ReactContextBaseJavaModule {
+  public PushNotificationModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  public String getName() {
+    return "PushNotificationModule";
+  }
+
+  @ReactMethod
+  public void showLog(String message) {
+    Log.d("PushNotificationModule", message);
+  }
+}
